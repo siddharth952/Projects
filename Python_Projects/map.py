e25a4052b42cdcf841bdee289c8efdf9912c186f
Siddharth <siddharth952@gmail.com>
2019-07-16 13:24:37 +0530

    Added Layers

--- Python_Projects/map.py
+++ Python_Projects/map.py
@@ -21,19 +21,29 @@ tileset_id = "http://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token
 
 mymap = folium.Map(location=[38.58, -99.09], zoom_start=6,tiles= tileset_id, attr="any text here")
 
-#Feature Group
-fg = folium.FeatureGroup(name= "My Map")
+#Feature Groups
+fgV = folium.FeatureGroup(name= "Volcanoes")
+fgP = folium.FeatureGroup(name= "Population")
 
+#MARK: Marker Layer
 #zip func to interate through two lists at the same time
 for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.CircleMarker(location = [lt,ln],radius= 5 ,popup=str(el)+"  m" , fill_color = color_gen(el),color = 'grey', fill_opacity = 0.7  ))
+    fgV.add_child(folium.CircleMarker(location = [lt,ln],radius= 5 ,popup=str(el)+"  m" , fill_color = color_gen(el),color = 'grey', fill_opacity = 0.7  ))
     
+#MARK: Json Layer
+fgP.add_child(folium.GeoJson(data = open('world.json','r', encoding = 'utf-8-sig').read(),
+style_function= lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000
+else 'red'
+}
+))
 
-fg = add_child(folium.GeoJson(data = (open('world.json','r'))))
 
 
-mymap.add_child(fg)
 
+mymap.add_child(fgV)
+mymap.add_child(fgP)
+mymap.add_child(folium.LayerControl())
 mymap.save("Mapfile.html")
 
 
